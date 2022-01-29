d2e9ba981d151bf7bcae35e3efab346b60d1e88a
Vishal Singh <vsing8298@gmail.com>
2022-01-29 13:25:31 +0530

    adding some more files

---
+++ Main.java
@@ -0,0 +1,28 @@
+class Player{
+	String name;
+	String country;
+	int noOfMatches;
+	int noOfWickets;
+	int noOfRuns;
+	public Player(String str1,String str2,int num1,int num2,int num3){
+		this.name=str1;
+		this.country=str2;
+		this.noOfMatches=num1;
+		this.noOfWickets=num2;
+		this.noOfWickets=num3;
+	}
+	public void displayPlayer(){
+		System.out.println(this.name);
+	}
+}
+class Team{
+	public Team(){
+		for
+	}
+}
+class Main{
+	public static void main(String[] args){
+		Team obj1=new Team();
+		obj1.displayPlayer();
+	}
+}
